7c8cc17af7f9e36bb41375f9b5c1355a3569695b
Mason Browne <mason.browne@gmail.com>
2018-05-17 22:11:23 -0700

    Wire up reducer

--- packages/perch-core/src/core/PCUIEditor.bs.js
+++ packages/perch-core/src/core/PCUIEditor.bs.js
@@ -1,9 +1,11 @@
 
 
+import * as Block from "bs-platform/lib/es6/block.js";
+import * as Curry from "bs-platform/lib/es6/curry.js";
 import * as React from "react";
 import * as ReasonReact from "reason-react/src/ReasonReact.js";
 
-var component = ReasonReact.statelessComponent("PCUIEditor");
+var component = ReasonReact.reducerComponent("PCUIEditor");
 
 function make(runtime, _) {
   return /* record */[
@@ -11,18 +13,34 @@ function make(runtime, _) {
           /* reactClassInternal */component[/* reactClassInternal */1],
           /* handedOffState */component[/* handedOffState */2],
           /* willReceiveProps */component[/* willReceiveProps */3],
-          /* didMount */component[/* didMount */4],
+          /* didMount */(function (self) {
+              setInterval((function () {
+                      return Curry._1(self[/* send */3], /* Bump */0);
+                    }), 1000);
+              return /* () */0;
+            }),
           /* didUpdate */component[/* didUpdate */5],
           /* willUnmount */component[/* willUnmount */6],
           /* willUpdate */component[/* willUpdate */7],
           /* shouldUpdate */component[/* shouldUpdate */8],
-          /* render */(function () {
+          /* render */(function (self) {
               var v = runtime[/* version */0];
-              return React.createElement("div", undefined, "Version: " + (String(v) + ""));
+              var bumps = self[/* state */1][/* bumps */1];
+              return React.createElement("div", undefined, "Version: " + (String(v) + ("; Bumps: " + (String(bumps) + " "))));
             }),
-          /* initialState */component[/* initialState */10],
+          /* initialState */(function () {
+              return /* record */[
+                      /* appSpecification */"",
+                      /* bumps */0
+                    ];
+            }),
           /* retainedProps */component[/* retainedProps */11],
-          /* reducer */component[/* reducer */12],
+          /* reducer */(function (_, state) {
+              return /* Update */Block.__(0, [/* record */[
+                          /* appSpecification */state[/* appSpecification */0],
+                          /* bumps */state[/* bumps */1] + 1 | 0
+                        ]]);
+            }),
           /* subscriptions */component[/* subscriptions */13],
           /* jsElementWrapped */component[/* jsElementWrapped */14]
         ];
--- packages/perch-core/src/core/PCUIEditor.re
+++ packages/perch-core/src/core/PCUIEditor.re
@@ -1,9 +1,25 @@
-let component = ReasonReact.statelessComponent("PCUIEditor");
+type state = {
+  appSpecification: string,
+  bumps: int,
+};
+
+type action =
+  | Bump;
+
+let component = ReasonReact.reducerComponent("PCUIEditor");
 
 let make = (~runtime: PCRuntime.t, _children) => {
   ...component,
-  render: _self => {
-    let v = runtime.version;
-    <div> (ReasonReact.string({j|Version: $(v)|j})) </div>;
+  initialState: () => {appSpecification: "", bumps: 0},
+  reducer: (action: action, state: state) =>
+    switch (action) {
+    | Bump => ReasonReact.Update({...state, bumps: state.bumps + 1})
+    },
+  didMount: self =>
+    Js.Global.setInterval(() => self.send(Bump), 1000) |> ignore,
+  render: self => {
+    let v = runtime.version
+    and bumps = self.state.bumps;
+    <div> (ReasonReact.string({j|Version: $(v); Bumps: $(bumps) |j})) </div>;
   },
 };
